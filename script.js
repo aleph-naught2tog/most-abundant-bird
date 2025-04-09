f9b38c9ed20f2dff19e8a10d701593d3e59a5071
Glitch (most-abundant-bird) <none>
2025-04-09 16:07:15 +0000

    Create maximum data objects

--- script.js
+++ script.js
@@ -30,19 +30,52 @@ function toColumns() {
   columnCount = table.getColumnCount();
   rowCount = table.getRowCount();
 
-  let columnarData = [];
+  const columnarData = [];
 
   for (let columnIndex = 0; columnIndex < columnCount; columnIndex += 1) {
     let currentColumns = [];
 
     for (let rowIndex = 0; rowIndex < rowCount; rowIndex += 1) {
-      const datum = table.get(rowIndex, columnIndex);
-      console.debug({ datum });
+      let datum = table.get(rowIndex, columnIndex);
+      
+      // this SHOULD just work where we parseFloat only if index > 0, but it... doesn't
+      if (rowIndex === 0) {
+        // // if we throw the datum here, we DO get the name of the bird
+        // throw new Error(datum)
+        // // if we do NOTHING, we get NaN, implying that we tried to parse the bird name
+      } else {
+        // when we log, we only ever see the numbers....
+        // console.debug(datum);
+        // 10 represents the base
+        datum = parseFloat(datum, 10);
+      }
+      
+      // when we do this, we only ever see the very first row's bird name
+      // and then the rest of the time it's NaN
+      isNaN(datum) && console.log({ datum });
+      
       currentColumns.push(datum);
     }
 
     columnarData.push(currentColumns);
   }
+  
+  const [birdNames, ...columns] = columnarData;
 
-  console.debug({ columnarData });
+  // skip the first column, it's all bird names
+  for (const col of columns) {
+    // find the index of the max
+    // get the max
+    // get the bird name for which the index corresponds
+    const maximum = col.reduce(({ maximum, maximumIndex, birdName }, currentValue, currentIndex) => {
+      if (currentValue >= maximum) {
+        return {maximum: currentValue, maximumIndex: currentIndex, birdName: birdNames[currentIndex]};
+      } else {
+        return {maximum, maximumIndex, birdName };
+      }
+    }, {maximum: -1, maximumIndex: -1, birdName: null});
+    
+    // `birdName` is NaN
+    console.debug({ maximum });
+  }
 }
