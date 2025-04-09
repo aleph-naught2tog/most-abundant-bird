21f6a67497574a3c094d8a71e4ae99cdb96734f8
Glitch (most-abundant-bird) <none>
2025-04-09 15:01:06 +0000

    Set up columnar data

--- script.js
+++ script.js
@@ -4,10 +4,45 @@
 // graph that (start with bar, then radial)
 // stretch: bird name on hover
 
-let daily = [];
+let table;
+let totalColumnCount;
+let dataColumnCount;
+let maxAbundanceData;
+
+// -----------------------------------
+// ------- Lifecycle functions -------
+// -----------------------------------
+
+function preload() {
+  table = loadTable("./wi_histogram.tsv", "TSV", toColumns);
+}
 
 function setup() {
   const canvas = createCanvas(600, 400);
   canvas.parent("canvas_container");
-  background('plum');
+}
+
+// -----------------------------------
+// -------- Custom functions ---------
+// -----------------------------------
+
+function toColumns() {
+  columnCount = table.getColumnCount();
+  rowCount = table.getRowCount();
+
+  let columnarData = [];
+
+  for (let columnIndex = 0; columnIndex < columnCount; columnIndex += 1) {
+    let currentColumns = [];
+
+    for (let rowIndex = 0; rowIndex < rowCount; rowIndex += 1) {
+      const datum = table.get(rowIndex, columnIndex);
+      console.debug({ datum });
+      currentColumns.push(datum);
+    }
+
+    columnarData.push(currentColumns);
+  }
+
+  console.debug({ columnarData });
 }
