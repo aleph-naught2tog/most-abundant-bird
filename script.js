f6e904e3d7732d2d296aecd9844300a0747093c6
Glitch (most-abundant-bird) <none>
2025-04-09 20:09:03 +0000

    A little clean up, prep for render

--- script.js
+++ script.js
@@ -4,78 +4,126 @@
 // graph that (start with bar, then radial)
 // stretch: bird name on hover
 
-let table;
-let totalColumnCount;
-let dataColumnCount;
-let maxAbundanceData;
+let maximumData;
+const EXCLUDE_BIRDS = [
+  "American Crow",
+  "American Robin",
+  "Black-capped chickadee",
+  "American goldfinch",
+  "blue jay",
+  "canada goose",
+];
 
 // -----------------------------------
 // ------- Lifecycle functions -------
 // -----------------------------------
 
 function preload() {
-  table = loadTable("./wi_histogram.tsv", "TSV", toColumns);
+  loadTable("./wi_histogram.tsv", "tsv", (data) => {
+    maximumData = toMaximumInfoColumns(data);
+  });
 }
 
 function setup() {
   const canvas = createCanvas(600, 400);
   canvas.parent("canvas_container");
+
+  renderBarChart();
 }
 
+
 // -----------------------------------
-// -------- Custom functions ---------
+// ---------- Render functions ---------
 // -----------------------------------
 
-function toColumns() {
-  columnCount = table.getColumnCount();
-  rowCount = table.getRowCount();
+// function renderBarChart() {
+//   for (const {  } of maximumData) {
+    
+//   }
+// }
+
+// -----------------------------------
+// ---------- Data functions ---------
+// -----------------------------------
+
+function toMaximumInfoColumns(tableData) {
+  const [birdNames, ...columns] = parseToColumns(tableData);
+
+  const maxInfo = columns.map((col) =>
+    calculateMaximumFromColumn(col, birdNames)
+  );
+  
+  console.debug({ maxInfo })
+
+  return maxInfo;
+}
+
+function parseToColumns(tableData) {
+  const columnCount = tableData.getColumnCount();
+  const rowCount = tableData.getRowCount();
 
   const columnarData = [];
 
-  for (let columnIndex = 0; columnIndex < columnCount; columnIndex += 1) {
+  for (let columnIndex = 0; columnIndex < columnCount - 1; columnIndex += 1) {
     let currentColumns = [];
 
     for (let rowIndex = 0; rowIndex < rowCount; rowIndex += 1) {
-      let datum = table.get(rowIndex, columnIndex);
-      
-      // this SHOULD just work where we parseFloat only if index > 0, but it... doesn't
-      if (rowIndex === 0) {
-        // // if we throw the datum here, we DO get the name of the bird
-        // throw new Error(datum)
-        // // if we do NOTHING, we get NaN, implying that we tried to parse the bird name
+      let datum = tableData.get(rowIndex, columnIndex);
+
+      if (columnIndex > 0) {
+        const datumAsFloat = parseFloat(datum, 10);
+        // idea: let user pick maximum abundance
+        // if (datumAsFloat > 0.5) {
+        //   continue;
+        // }
+        
+        currentColumns.push(datumAsFloat);
       } else {
-        // when we log, we only ever see the numbers....
-        // console.debug(datum);
-        // 10 represents the base
-        datum = parseFloat(datum, 10);
+        // // idea: let user pick which birds to exclude
+        // if (EXCLUDE_BIRDS.some(ebn => datum.toLowerCase().startsWith(ebn.toLowerCase()))) {
+        //   continue;
+        // }
+        
+        currentColumns.push(datum);
       }
-      
-      // when we do this, we only ever see the very first row's bird name
-      // and then the rest of the time it's NaN
-      isNaN(datum) && console.log({ datum });
-      
-      currentColumns.push(datum);
     }
 
     columnarData.push(currentColumns);
   }
-  
-  const [birdNames, ...columns] = columnarData;
 
-  // skip the first column, it's all bird names
-  for (const col of columns) {
-    // find the index of the max
-    // get the max
-    // get the bird name for which the index corresponds
-    const maximum = col.reduce(({ maximum, maximumIndex, birdName }, currentValue, currentIndex) => {
+  return columnarData;
+}
+
+function calculateMaximumFromColumn(col, birdNames) {
+  return col.reduce(
+    ({ maximum, maximumIndex, birdName }, currentValue, currentIndex) => {
       if (currentValue >= maximum) {
-        return {maximum: currentValue, maximumIndex: currentIndex, birdName: birdNames[currentIndex]};
+        return {
+          maximum: currentValue,
+          maximumIndex: currentIndex,
+          birdName: birdNames[currentIndex],
+        };
       } else {
-        return {maximum, maximumIndex, birdName };
+        return { maximum, maximumIndex, birdName };
       }
-    }, {maximum: -1, maximumIndex: -1, birdName: null});
-    
-    // `birdName` is NaN
-    console.debug({ maximum });
-  }
+    },
+    { maximum: -1, maximumIndex: -1, birdName: null }
+  );
+}
+
+// this is just to see if there are any that just dominate everything
+// like, say, a goose
+function groupByBirdName(columnMaxes) {
+  const result = columnMaxes.reduce((birdGrouping, currentMaxInfo) => {
+    // I hate how the negation of `in` looks
+    if (!(currentMaxInfo.birdName in birdGrouping)) {
+      birdGrouping[currentMaxInfo.birdName] = 0;
+    }
+
+    birdGrouping[currentMaxInfo.birdName] += 1;
+
+    return birdGrouping;
+  }, {});
+  
+  return result;
 }
