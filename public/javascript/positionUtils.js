6797d1b8bcc77c3d231aabcc8fb2ecb4f7508fd9
Max Cerrina <mrcerrina@gmail.com>
2025-04-13 08:54:10 -0500

    Add back graphing utils

---
+++ public/javascript/positionUtils.js
@@ -0,0 +1,60 @@
+function drawProbablyGreenCanvasPoint({ x, y }, desiredColor) {
+  push();
+  strokeWeight(15);
+  const strokeC = desiredColor ?? color(0, 255, 0, 120);
+  stroke(strokeC);
+
+  point(x, y);
+  pop();
+}
+
+function drawProbablyBlueCirclePoint({ x, y }, desiredColor) {
+  push();
+  strokeWeight(25);
+  const strokeC = desiredColor ?? color(0, 0, 255, 60);
+  stroke(strokeC);
+
+  point(x, y);
+  pop();
+}
+
+const vecToPoint = ([x, y]) => ({ x, y });
+
+function drawGrid(gridCount) {
+  push();
+
+  stroke('darkgray');
+  strokeWeight(2);
+
+  for (let x = 0; x <= width; x += width / gridCount) {
+    for (let y = 0; y <= width; y += height / gridCount) {
+      line(x, 0, x, height);
+      line(0, y, height, y);
+    }
+  }
+
+  pop();
+}
+
+function drawCoordinatePoints(strokeColor) {
+  push();
+
+  stroke(strokeColor);
+  strokeWeight(5);
+
+  text('x=0,y=0', 0, 0);
+  text('x=1,y=0', 100, 0);
+  text('x=0,y=1', 0, 100);
+  text('x=-1,y=0', -100, 0);
+  text('x=0,y=-1', 0, -100);
+
+  pop();
+}
+
+function drawOrigin() {
+  push();
+  noStroke()
+  fill('orange');
+  circle(0, 0, 4);
+  pop();
+}
